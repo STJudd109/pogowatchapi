0dd88a09d19126c6755a9779b4e936eaf33d6b54
Spencer Judd <spencerjfam@hotmail.com>
2016-07-28 13:31:38 -0600

    fix

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.get('/api/account', function (req, res) {
         console.log('1[i] Poke Storage: ' + profile.poke_storage);
         console.log('1[i] Item Storage: ' + profile.item_storage);
 
-        res.send({ "username": profile.username, "storage": profile.poke_storage, "istorage": profile.item_storage, "stardust": profile.currency[1].amount, "pokecoins": profile.currency[0].amount});
+        res.sendStatus({ "username": profile.username, "storage": profile.poke_storage, "istorage": profile.item_storage, "stardust": profile.currency[1].amount, "pokecoins": profile.currency[0].amount});
 
         var poke = 0;
         if (profile.currency[0].amount) {
