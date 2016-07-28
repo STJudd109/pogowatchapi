61f007e71af524f4a348ff3883a185e79cd965b7
Spencer Judd <spencerjfam@hotmail.com>
2016-07-28 14:33:33 -0600

    Login fix 3

--- app.js
+++ app.js
@@ -63,15 +63,16 @@ function unauthorized(res){
 //   });
 // });
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
 app.enable('trust proxy');
 // app.set('trust proxy', 'loopback');
 
 
-app.get('/api/account', function (req, res) {
+app.post('/api/account', function (req, res) {
     // var user = basicAuth(req);
     // if(!user || !user.name || !user.pass){
     //   return unauthorized(res);
